ad252b9d945bb9756903806050df9cb6dc18e53d
VaidehiRami <ramivaidehi@gmail.com>
2022-06-01 12:16:59 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const inputname=useRef();
+
+  const submitHendler= async ()=>{
+    const name =inputname.current.value;
+   
+    await fetch('https://fir-da7eb-default-rtdb.firebaseio.com/UserName.json',{
+      method:'POST',
+      body:JSON.stringify({inp:name}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+  }
+  const fetchData = async()=>{
+   const res = await fetch('https://fir-da7eb-default-rtdb.firebaseio.com/UserName.json')
+    const data =await res.json();
+    const fetchName=[];
+    for (const key in data) {
+      fetchName.push({
+        id: key,
+        name:data.inp,
+      });
+    }
+    console.log(data);
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" ref={inputname}/>
+      <button onClick={fetchData}>Fetch Data</button>
+      <button onClick={submitHendler}>Submit Data</button>
+
+      
     </div>
   );
 }
